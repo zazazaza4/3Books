b228d72a32c7b4b00bc36ed1d9eafac975bdead0
Yaroslav <6715414@gmail.com>
2023-07-23 13:45:15 +0300

    books-3: + additional icons

--- client/src/libs/components/icon/common.js
+++ client/src/libs/components/icon/common.js
@@ -1,8 +1,19 @@
-import { faUser, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCircleNotch,
+  faImage,
+  faLock,
+  faSignOutAlt,
+  faUser,
+  faUserCircle
+} from '@fortawesome/free-solid-svg-icons';
 
 import { IconName } from '~/libs/enums/enums.js';
 
 const iconNameToSvgIcon = {
+  [IconName.IMAGE]: faImage,
+  [IconName.LOCK]: faLock,
+  [IconName.LOG_OUT]: faSignOutAlt,
+  [IconName.SPINNER]: faCircleNotch,
   [IconName.USER]: faUser,
   [IconName.USER_CIRCLE]: faUserCircle
 };
