60e7ad3f4ff4f46cb0d9299224adb9b20af8e247
Yaroslav <6715414@gmail.com>
2023-07-27 15:15:26 +0300

    books-0: + additional routes

--- client/src/libs/enums/app/app-route.enum.js
+++ client/src/libs/enums/app/app-route.enum.js
@@ -1,11 +1,16 @@
 const AppRoute = {
   ROOT: '/',
+  BOOKS: '/tags',
   BOOK: '/books/:book',
   READER: '/reader/:book',
   ANY: '*',
   LOGIN: '/login',
   REGISTRATION: '/registration',
-  PROFILE: '/profile'
+  PROFILE: '/profile',
+  HELP: '/help',
+  ABOUT: '/about',
+  CONTACT: '/contact',
+  PROFILE_EDIT: '/profile/edit'
 };
 
 export { AppRoute };
